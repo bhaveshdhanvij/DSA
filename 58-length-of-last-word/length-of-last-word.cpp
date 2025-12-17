7d7d2cd915c43b9e6f627eeb913b253274723ce9
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2025-12-17 10:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.69%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0 ;
-        int n = s.size() ;
-        string w = "" ;
-        for ( int i = 0 ; i < n ; ) {
-            while ( i < n && s[i] == ' ' ) i++ ;
-            while ( i < n && s[i] != ' ' ) {
-                w += s[i] ;
-                i++ ;
-            }
-            if ( !w.empty() ) ans = w.length() ;
-            w.clear() ;
+        // int ans = 0 ;
+        // int n = s.size() ;
+        // string w = "" ;
+        // for ( int i = 0 ; i < n ; ) {
+        //     while ( i < n && s[i] == ' ' ) i++ ;
+        //     while ( i < n && s[i] != ' ' ) {
+        //         w += s[i] ;
+        //         i++ ;
+        //     }
+        //     if ( !w.empty() ) ans = w.length() ;
+        //     w.clear() ;
+        // }
+        // return ans ;
+
+
+        stringstream ss(s) ;
+        string word , ans ;
+        while ( ss >> word ) {
+            ans = word ;
         }
-        return ans ;
+        return ans.length() ;
     }
 };
