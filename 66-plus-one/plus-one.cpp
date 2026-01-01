7e5af9bf73b5afcc3dddc485e5851de4ff6f9220
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-01-01 12:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (50.17%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& nums) {
+        for ( int i = nums.size() - 1 ; i >= 0 ; i-- ) {
+            if ( ++nums[i] < 10 ) {
+                return nums ;
+            }
+            nums[i] = 0 ;
+        }
+        nums.insert(nums.begin(), 1) ;
+        return nums ;
+    }
+};
