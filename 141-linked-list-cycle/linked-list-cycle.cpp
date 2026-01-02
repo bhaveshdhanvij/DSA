72fd9d8463b54a78f0e6ada3608379905e98282e
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-01-02 09:45:37 +0530

    Time: 9 ms (47.81%) | Memory: 11.8 MB (79.36%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,13 +9,24 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // Approach 1 : HashMap ( Trace Appearance ) 
-        unordered_map<ListNode*,int> umap ;
-        ListNode* curr = head ;
-        while ( curr ) {
-            if ( umap.count(curr) ) return true ;
-            umap[curr]++ ;
-            curr = curr->next ;
+        // // Approach 1 : HashMap ( Trace Appearance ) 
+        // unordered_map<ListNode*,int> umap ;
+        // ListNode* curr = head ;
+        // while ( curr ) {
+        //     if ( umap.count(curr) ) return true ;
+        //     umap[curr]++ ;
+        //     curr = curr->next ;
+        // }
+        // return false ;
+
+
+        // Approach 2 : Floyd Warshall's Appearance 
+        ListNode* fast = head ;
+        ListNode* slow = head ;
+        while ( fast && fast->next ) {
+            slow = slow->next ;
+            fast = fast->next->next ;
+            if ( slow == fast ) return true ;
         }
         return false ;
     }
