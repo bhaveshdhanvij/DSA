382f1b8681633ad528de192c2651713a86838ec9
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-01-15 14:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 93.9 MB (87.27%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dfs( int r , int c , vector<vector<int>>& grid , int delRow[] , int delCol[] ) {
+        int total = grid[r][c] ;
+        grid[r][c] = 0 ;
+
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+
+        for ( int i = 0 ; i < 4 ; i++ ) {
+            int nr = r + delRow[i] ;
+            int nc = c + delCol[i] ;
+            
+            if ( nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] != 0 ) {
+                total += dfs ( nr , nc , grid , delRow , delCol ) ;
+            }
+        } 
+        return total ;
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        
+        int delRow[] = {-1 , 0 , 1 , 0} ;
+        int delCol[] = {0 , 1 , 0 , -1} ;
+
+        int ans = 0 ;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < m ; j++ ) {
+                if ( grid[i][j] != 0 ) {
+                    int curr = dfs( i , j , grid , delRow , delCol ) ;
+                    ans = max ( ans , curr ) ;
+                }
+            }
+        }
+        
+        return ans ;
+    }
+};
