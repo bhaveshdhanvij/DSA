6d8194e2ac78303784e9ee36b34986ed47f0cf77
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-01-18 15:46:06 +0530

    Time: 4 ms (80.01%) | Memory: 15.3 MB (24.60%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st ;
+        int num = 0 ;
+        char op = '+' ;
+
+        for ( int i = 0 ; i < s.size() ; i++ ) {
+            char c = s[i] ;
+            if ( isdigit(c) ) {
+                num = num * 10 + (c - '0') ;
+            }
+
+            if ( (!isdigit(c) && c != ' ') || i == s.size() - 1 ) {
+                if ( op == '+' ) {
+                    st.push(num) ;
+                } else if ( op == '-' ) {
+                    st.push(-num) ;
+                }else if ( op == '*' ) {
+                    int top = st.top() ;
+                    st.pop() ;
+                    st.push(top * num) ;
+                }else if ( op == '/' ) {
+                    int top = st.top() ;
+                    st.pop() ;
+                    st.push(top / num) ;
+                }
+
+                num = 0 ;
+                op = c ;
+            }
+        }
+
+        int ans = 0 ; 
+
+        while ( !st.empty() ) {
+            ans += st.top() ;
+            st.pop() ;
+        }
+
+        return ans ;
+    }
+};
