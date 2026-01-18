f1ddf65bb05e191a300e6564531a756ffe0a0176
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-01-18 15:48:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (10.43%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st ;
+
+        for ( string s : logs ) {
+            if ( s == "../" ) {
+                if ( !st.empty() ) st.pop() ;
+            } else if ( s != "./" ) {
+                st.push(s) ;
+            }
+        }
+
+        return st.size() ;
+    }
+};
