21f6f33bc1fdd4c3bf0ccf9c9fe63285a3e3c8a7
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-02-18 11:10:25 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (47.76%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -24,15 +24,13 @@ public:
 
         int n = a.size() ;
         int i = 0 , j = n - 1 ;
-
         int ans = 0 ;
-        
+
         while ( i < j ) {
             int h = min ( a[i] , a[j] ) ;
             int w = j - i ;
 
             int curr = h * w ;
-
             ans = max ( ans , curr ) ;
 
             if ( a[i] <= a[j] ) i++ ;
