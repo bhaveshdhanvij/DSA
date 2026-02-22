1848b520a72853f46fd9b2c39d894a72205f0005
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-02-22 21:01:14 +0530

    Time: 8 ms (10.40%) | Memory: 8.4 MB (91.35%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if ( x < 0 ) return false ;
+        long long rev = 0 , temp = x ;
+        while ( temp > 0 ) {
+            int d = temp % 10 ;
+            rev = rev * 10 + d ;
+            temp /= 10 ;
+        }
+        return rev == x ;
+    }
+};
