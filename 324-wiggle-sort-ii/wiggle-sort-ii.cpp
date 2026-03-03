b50e8172a031478fb38b46ed69bb5641291f2c71
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-03 21:00:35 +0530

    Time: 15 ms (5.41%) | Memory: 22.7 MB (5.73%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& a) {
+        int n = a.size() ;
+
+        priority_queue<int> pq ;
+        for ( int x : a ) pq.push(x) ;
+
+        for ( int i = 1 ; i < n ; i += 2 ) {
+            a[i] = pq.top() ;
+            pq.pop() ;
+        }
+
+        for ( int i = 0 ; i < n ; i += 2 ) {
+            a[i] = pq.top() ;
+            pq.pop() ;
+        }
+    }
+};
