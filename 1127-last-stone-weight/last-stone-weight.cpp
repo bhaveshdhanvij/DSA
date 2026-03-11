73b5f98ed84c4ddd5261360b284cbadd3c566b1b
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-11 22:43:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (79.13%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        // Optimal Approach : Using Max Heap 
-        // TC : O(NlogN) , SC : O(N) 
-        
+    int lastStoneWeight(vector<int>& a) {
         priority_queue<int> pq ;
-        for ( int x : stones ) pq.push(x) ;
-        while (pq.size() > 1) {
+        for ( int x : a ) pq.push(x) ;
+
+        while ( pq.size() > 1 ) {
             int x = pq.top() ; pq.pop() ;
             int y = pq.top() ; pq.pop() ;
+
             if ( x != y ) {
                 pq.push(x - y) ;
             }
         }
+
         return pq.empty() ? 0 : pq.top() ;
     }
 };
