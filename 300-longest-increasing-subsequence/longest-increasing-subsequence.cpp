265213a58fbe9d3b9fef6484884f9ce00a5bf0a3
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-18 19:30:44 +0530

    Time: 53 ms (66.06%) | Memory: 14.5 MB (45.60%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& a) {
+        // Approach 1 : brute force check every sub. and check if that is increasing or not  
+        // TC : O(2**N * N)
+
+        // Optimal Approach : Memoization DP 
+        // dp[i] = will store maximum sub. length ending at a[i] 
+        // TC : O(N**2) SC : O(N)
+
+        int n = a.size() ;
+        
+        vector<int> dp(n , 1) ;
+        int ans = 1 ;
+        
+        for ( int i = 1 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < i ; j++ ) {
+                if ( a[i] > a[j] ) {
+                    dp[i] = max ( dp[i] , dp[j] + 1 ) ;
+                }
+            }
+            ans = max ( ans , dp[i] ) ;
+        }
+
+        return ans ;
+    }
+};
