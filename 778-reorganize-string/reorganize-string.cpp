292071f93004136e97c13031158c6b86bd1d7c01
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-21 11:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (53.21%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        int c[26] = {0} ;
-        for ( char ch : s ) c[ch - 'a']++ ;
-
+        int n = s.size() ;
+        unordered_map<char,int> mp ;
         priority_queue<pair<int,char>> pq ;
-        for ( int i = 0 ; i < 26 ; i++ ) {
-            if ( c[i] != 0 ) pq.push({c[i] , i + 'a'}) ;
+
+        for ( char c : s ) mp[c]++ ;
+        for ( auto p : mp ) {
+            if ( p.second > (n + 1) / 2 ) return "" ;
+            pq.push({p.second , p.first}) ;
         }
 
         string ans = "" ;
-        pair<int,char> prev = pq.top() ;
+        auto prev = pq.top() ;
         pq.pop() ;
-
         ans += prev.second ;
         prev.first-- ;
 
-        while ( !pq.empty() ) {
-            pair<int,char> next = pq.top() ;
+        while (!pq.empty()) {
+            auto curr = pq.top() ;
             pq.pop() ;
 
-            ans += next.second ;
-            next.first-- ;
+            ans += curr.second ;
+            curr.first-- ;
 
-            if ( prev.first > 0 ) {
-                pq.push(prev) ;
-            }
-
-            prev = next ;
+            if ( prev.first > 0 ) pq.push(prev) ;
+            prev = curr ;
         }
 
-        if ( prev.first > 0 ) return "" ;
         return ans ;
     }
 };
