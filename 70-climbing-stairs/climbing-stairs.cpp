528bbaf4f4c082704fe027dbfc9fd3159d61edfb
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-24 16:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1 , 0) ;
-        dp[0] = dp[1] = 1 ;
-        for ( int i = 2 ; i <= n ; i++ ) {
+        // TC : O(N) , SC : O(N)
+        if ( n <= 1 ) return 1 ;
+        vector<int> dp(n + 1, 0) ;
+        
+        // dp[i] will store distinct ways to reach ith stair
+        dp[1] = 1 ;
+        dp[2] = 2 ;
+
+        for ( int i = 3 ; i <= n ; i++ ) {
             dp[i] = dp[i - 1] + dp[i - 2] ;
         }
 
