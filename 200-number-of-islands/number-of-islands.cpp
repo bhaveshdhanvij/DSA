52276d9d77c8dae2f407ca32aa890d6a3d91be48
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-26 23:02:18 +0530

    Time: 20 ms (94.34%) | Memory: 16.3 MB (77.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,34 @@
 class Solution {
-private:
-    void dfs ( int r , int c, vector<vector<char>> &grid , int delRow[] , int delCol[] ) {
-        grid[r][c] = '0' ;
-        
-        int n = grid.size() ;
-        int m = grid[0].size() ;
-
-        for ( int i = 0 ; i < 4 ; i++ ) {
-            int nr = r + delRow[i] ;
-            int nc = c + delCol[i] ;
-            
-            if ( nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' ) {
-                dfs(nr , nc , grid , delRow , delCol) ;
-            }
-        }
-    } 
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size() ;
-        int m = grid[0].size() ;
+    void helper( int i , int j , vector<vector<char>>& a, int delRow[] , int delCol[] ) {
+        int n = a.size() , m = a[0].size() ;
+        if ( i < 0 || i >= n || j < 0 || j >= m || a[i][j] == '0' ) return ;
 
-        int ans = 0 ;
+        a[i][j] = '0' ;
+        for ( int k = 0 ; k < 4 ; k++ ) {
+            int nr = i + delRow[k] ;
+            int nc = j + delCol[k] ;
+            helper( nr , nc , a , delRow , delCol) ;
+        }
+    }
+    int numIslands(vector<vector<char>>& a) {
+        // TC : O(N * M) , SC : O(N * M)
+        // Optimal Approach : Using DFS to count the no of groups and flood filling 
+        int n = a.size() , m = a[0].size() ;
+        int c = 0 ; // no of islands
 
         int delRow[] = {-1 , 0 , 1 , 0} ;
         int delCol[] = {0 , 1 , 0 , -1} ;
 
         for ( int i = 0 ; i < n ; i++ ) {
             for ( int j = 0 ; j < m ; j++ ) {
-                if ( grid[i][j] == '1' ) {
-                    ans++ ;
-                    dfs(i , j , grid , delRow , delCol) ;
+                if ( a[i][j] == '1' ) {
+                    c++ ;
+                    helper(i , j , a , delRow , delCol) ;
                 }
             }
         }
 
-        return ans ;
+        return c ;
     }
 };
