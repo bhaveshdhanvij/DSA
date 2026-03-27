fa886ba315993c269aa028576f725303451e3bd1
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-03-27 08:32:56 +0530

    Time: 15 ms (92.08%) | Memory: 17.9 MB (70.49%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // Optimal Approach : unbounded knapsack 
+        // Tabulation from bottom up build up for every amount 
+
+        vector<int> dp(amount + 1 , 1e9) ;
+        dp[0] = 0 ;
+
+        for ( int i = 1 ; i <= amount ; i++ ) {
+            for ( int c : coins ) {
+                if ( i - c >= 0 ) {
+                    dp[i] = min ( dp[i] , dp[i - c] + 1 ) ;
+                }
+            }
+        }
+
+        return dp[amount] == 1e9 ? -1 : dp[amount] ;
+    }
+};
