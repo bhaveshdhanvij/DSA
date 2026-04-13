bf650f56fee8277ba7c44f8b02e113a86225eaf9
Bhavesh Dhanvij <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-04-13 21:23:35 +0530

    Time: 9 ms (16.97%) | Memory: 19.1 MB (22.17%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& p) {
+        // just track the path while applying topological sort 
         vector<int> ans ;
-        queue<int> q ;
-        int c = 0 ;
-
-        vector<int> indeg(n , 0) ;
         vector<vector<int>> a(n) ;
+        vector<int> indeg(n , 0) ;
 
-        for ( auto e : p ) {
-            a[e[1]].push_back(e[0]) ;
-            indeg[e[0]]++ ;
+        for ( auto x : p ) {
+            a[x[1]].push_back(x[0]) ;
+            indeg[x[0]]++ ;
         }
 
+        queue<int> q ;
         for ( int i = 0 ; i < n ; i++ ) {
-            if ( indeg[i] == 0 ) {
-                q.push(i) ;
-                ans.push_back(i) ;
-            }
+            if ( indeg[i] == 0 ) q.push(i) ;
         }
 
-        while (!q.empty()) {
-            int node = q.front() ;
+        int count = 0 ;
+        while ( !q.empty() ) {
+            int curr = q.front() ;
             q.pop() ;
-            c++ ;
 
-            for ( int x : a[node] ) {
+            ans.push_back(curr) ;
+            count++ ;
+
+            for ( int x : a[curr] ) {
                 --indeg[x] ;
-                if ( indeg[x] == 0 ) {
-                    q.push(x) ;
-                    ans.push_back(x) ;
-                }
+                if (indeg[x] == 0) q.push(x) ;
             }
         }
-        vector<int> t ;
-        return c == n ? ans : t ;
+
+        vector<int> temp ;
+        return count == n ? ans : temp ;
     }
 };
