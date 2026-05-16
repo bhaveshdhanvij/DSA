bb095052f3475907be1c76fae6620bf6a444de5f
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-16 14:08:14 +0530

    Time: 3 ms (32.60%) | Memory: 7.6 MB (99.60%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long l = 0 , r = sqrt(c) ;
+        
+        while ( l <= r ) {
+            long long sum = l * l + r * r ;
+            if ( sum == c ) {
+                return true ;
+            }else if ( sum < c ) {
+                l++ ;
+            }else {
+                r-- ;
+            }
+        }
+
+        return false ;
+    }
+};
