6f3bd502152c4c015545321a00764470077fac6c
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-19 15:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (97.72%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getCommon(vector<int>& a, vector<int>& b) {
+        int n = a.size() , m = b.size() ;
+        int i = 0 , j = 0 ;
+        while ( i < n && j < m ) {
+            if ( a[i] == b[j] ) return a[i] ;
+            else if ( a[i] < b[j] ) i++ ;
+            else j++ ;
+        }
+        return -1 ;
+    }
+};
