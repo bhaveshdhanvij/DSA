adfb20513c088c2a3f4fed6a7858aad2f4ee7b76
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-23 14:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (6.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& a, int target) {
-        int n = a.size() , m = a[0].size() ;
-        // Approach 1 : traverse the entire matrix and check if the target is present or not 
-        // Approach 2 : for each row check if target lies in that range , if yes binary search on that row since it's sorted 
-        // Optimal Approach : Imagine the 2-d array into 1-d array and use manipulation technique of indexing in terms of row and column no. of the matrix 
-        // TC : O(log(N * M)) , SC : O(1) 
-
-        int l = 0 , h = n * m - 1 ;
-        while ( l <= h ) {
-            int mid = (l + h) / 2 ;
-            
-            int row = mid / m ;
-            int col = mid % m ;
-
-            if ( a[row][col] == target ) {
+    // Cisco
+    // Uber
+    // Visa
+    // Amazon
+    // Goldman Sachs
+    // Meta
+    // Apple
+    bool search ( vector<int> nums , int t ) {
+        int n = nums.size() ;
+        int l = 0 , r = n - 1 ;
+        while ( l <= r ) {
+            int mid = l + (r - l) / 2 ;
+            if ( nums[mid] == t ) {
                 return true ;
-            }else if ( a[row][col] < target ) {
+            }else if ( nums[mid] < t ) {
                 l = mid + 1 ;
             }else {
-                h = mid - 1 ;
+                r = mid - 1 ;
             }
         }
+        return false ;
+    }
+    bool searchMatrix(vector<vector<int>>& a, int target) {
+        // Approach 1 : traverse the entire matrix and check whether it is present or not 
+        // Approach 2 : traverse via each row and check if target exists in it , if yes binary search on it 
 
+        int n = a.size() ;
+        for ( int i = 0 ; i < n ; i++ ) {
+            if ( target >= a[i].front() && target <= a[i].back() ) {
+                return search(a[i] , target) ;
+            }
+        }
         return false ;
     }
 };
