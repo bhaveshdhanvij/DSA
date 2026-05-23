944daa328c12a8b1d3693cc7b81f61b460220742
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-23 12:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (47.36%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,44 @@
 class Solution {
 public:
+    // MakeMyTrip
+    // Amazon
+    // Microsoft
     void sortColors(vector<int>& nums) {
-        sort ( nums.begin() , nums.end() ) ;
-        
+        // Approach 1 : Two pass sort : first pass : move 0's to front and then move 1's after that
+        // TC : O(N) , SC : O(1) 
+
+        // int n = nums.size() ;
+        // int i = -1 ;
+        // for ( int j = 0 ; j < n ; j++ ) {
+        //     if ( nums[j] == 0 ) {
+        //         i++ ;
+        //         swap(nums[i] , nums[j]) ;
+        //     }
+        // }
+        // int k = i + 1 ; // search for 1 now from i + 1 
+        // for ( int j = k ; j < n ; j++ ) {
+        //     if ( nums[j] == 1 ) {
+        //         i++ ;
+        //         swap(nums[i] , nums[j]) ;
+        //     }
+        // }
+
+        // Optimal Approach : Dutch National Flag Algorithm : using three pointers low , mid , high 
+        // TC : O(N) , SC : O(1) 
+
+        int n = nums.size() ;
+        int low = 0 , mid = 0 , high = n - 1 ;
+        while ( mid <= high ) {
+            if ( nums[mid] == 0 ) {
+                swap(nums[low] , nums[mid]) ;
+                low++ ;
+                mid++ ;
+            }else if ( nums[mid] == 1 ) {
+                mid++ ;
+            }else {
+                swap(nums[mid] , nums[high]) ;
+                high-- ;
+            }
+        }
     }
 };
