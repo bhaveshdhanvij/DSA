9aea7eb2335b2a9825213c6f00da68475a64caac
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-23 14:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (77.92%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -25,11 +25,33 @@ public:
     bool searchMatrix(vector<vector<int>>& a, int target) {
         // Approach 1 : traverse the entire matrix and check whether it is present or not 
         // Approach 2 : traverse via each row and check if target exists in it , if yes binary search on it 
+        // TC : O(N * log(N * M)) , SC : O(1) 
 
-        int n = a.size() ;
-        for ( int i = 0 ; i < n ; i++ ) {
-            if ( target >= a[i].front() && target <= a[i].back() ) {
-                return search(a[i] , target) ;
+        // int n = a.size() ;
+        // for ( int i = 0 ; i < n ; i++ ) {
+        //     if ( target >= a[i].front() && target <= a[i].back() ) {
+        //         return search(a[i] , target) ;
+        //     }
+        // }
+        // return false ;
+
+        // Optimal Approach : imagine the 2d array as 1d array and use manipulation technique of indexing in terms of row and column no. of matrix 
+        // TC : O(log(N * M)) , SC : O(1) 
+
+        int n = a.size() , m = a[0].size() ;
+        int l = 0 , h = m * n - 1 ;
+        while ( l <= h ) {
+            int mid = (l + h) / 2 ;
+            
+            int row = mid / m ;
+            int col = mid % m ;
+
+            if ( a[row][col] == target ) {
+                return true ;
+            }else if ( a[row][col] < target ) {
+                l = mid + 1 ;
+            }else {
+                h = mid - 1 ;
             }
         }
         return false ;
