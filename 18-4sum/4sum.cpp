f02ae874902e7c34fe1eb86b91ba19aa0ca3ac70
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-23 13:41:56 +0530

    Time: 15 ms (90.58%) | Memory: 17.5 MB (20.88%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,41 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& a, int target) {
-        int n = a.size() ;
-        sort ( a.begin() , a.end() ) ;
-        set<vector<int>> s ;
-        
-        for ( int i = 0 ; i < n ; i++ ) {
-            for ( int j = i + 1 ; j < n ; j++ ) {
-                for ( int k = j + 1 ; k < n ; k++ ) {
-                    long t = target ;
-                    t -= a[i] ;
-                    t -= a[j] ;
-                    t -= a[k] ;
-                    // binary search to find the remaining sum 
-                    int l = k + 1 , h = n - 1 ;
-                    while ( l <= h ) {
-                        int mid = l + (h - l) / 2 ;
-                        if ( a[mid] == t ) {
-                            vector<int> b ;
-                            b.push_back(a[i]) ;
-                            b.push_back(a[j]) ;
-                            b.push_back(a[k]) ;
-                            b.push_back(a[mid]) ;
-                            s.insert(b) ;
-                            break ;
-                        }else if ( a[mid] < t ) {
-                            l = mid + 1 ;
-                        }else {
-                            h = mid - 1 ;
-                        }
+    // Product Based:-
+    // Adobe
+    // OYO
+    // Uber
+    // Microsoft
+    // Apple
+    // Rubrik
+    // Service Based:-
+    // TCS
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        // Brute Force approach : use 4 nested loops 
+        // TC : O(N ** 4) , SC : O(k) 
+
+        // Optimal Approach : using 2 loops + two pointers 
+        // TC : O(N ** 3) , SC : O(1) 
+
+        int n = nums.size() ;
+        vector<vector<int>> ans ;
+        sort ( nums.begin() , nums.end() ) ;
+
+        for ( int first = 0 ; first < n - 3 ; first++ ) {
+            if ( first > 0 && nums[first] == nums[first - 1] ) continue ;
+
+            for ( int second = first + 1 ; second < n - 2 ; second++ ) {
+                if ( second > first + 1 && nums[second] == nums[second - 1] ) continue ;
+
+                long long remtarget = (long long)target - nums[first] - nums[second] ;
+                int left = second + 1 , right = n - 1 ;
+
+                while ( left < right ) {
+                    int sum = nums[left] + nums[right] ;
+                    if ( sum == remtarget ) {
+                        ans.push_back({nums[first] , nums[second] , nums[left] , nums[right]}) ;
+
+
+                        while ( left < right && nums[left] == nums[left + 1] ) left++ ;
+                        while ( left < right && nums[right] == nums[right - 1] ) right-- ;
+
+                        left++ ;
+                        right-- ;
+                    }else if ( sum < remtarget ) {
+                        left++ ;
+                    }else {
+                        right-- ;
                     }
                 }
             }
         }
-        
-        vector<vector<int>> ans ( s.begin() , s.end() ) ;
-        
         return ans ;
-    }
+    }   
 };
