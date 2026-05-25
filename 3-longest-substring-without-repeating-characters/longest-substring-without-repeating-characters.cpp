bcdd0844ab0d3931164fe388d2eb5da7fe822bbb
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-25 19:09:41 +0530

    Time: 15 ms (38.10%) | Memory: 14.2 MB (28.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,15 +5,32 @@ public:
     int lengthOfLongestSubstring(string s) {
         // Approach 1 : Checking all substrings with HashSet 
         // TC : O(N * N) , SC : O(1) 
+
+        // int n = s.size() ;
+        // int ans = 0 ;
+        // for ( int i = 0 ; i < n ; i++ ) {
+        //     unordered_set<char> uset ;
+        //     for ( int j = i ; j < n ; j++ ) {
+        //         if ( uset.count(s[j]) ) break ; // this means duplicate found 
+        //         uset.insert(s[j]) ;
+        //         ans = max ( ans , j - i + 1 ) ;
+        //     }
+        // }
+        // return ans ;
+
+        // Approach 2 : Sliding Window + HashSet 
+        // TC : O(N) , SC : O(1) 
+
         int n = s.size() ;
-        int ans = 0 ;
-        for ( int i = 0 ; i < n ; i++ ) {
-            unordered_set<char> uset ;
-            for ( int j = i ; j < n ; j++ ) {
-                if ( uset.count(s[j]) ) break ; // this means duplicate found 
-                uset.insert(s[j]) ;
-                ans = max ( ans , j - i + 1 ) ;
+        int l = 0 , ans = 0 ;
+        unordered_set<char> uset ;
+        for ( int r = 0 ; r < n ; r++ ) {
+            while ( uset.count(s[r]) ) {
+                uset.erase(s[l]) ;
+                l++ ;
             }
+            uset.insert(s[r]) ;
+            ans = max ( ans , r - l + 1 ) ;
         }
         return ans ;
     }
