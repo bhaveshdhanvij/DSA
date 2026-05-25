2299274f912fcdb61e45aba31b6556f0dde45c41
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-25 19:06:18 +0530

    Time: 656 ms (5.00%) | Memory: 247.6 MB (5.17%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    // Amazon
+    // Morgan Stanley
     int lengthOfLongestSubstring(string s) {
-        // TC : O(N) , SC : O(26) 
-        int n = s.length() ;
-        int ans = 0 , l = 0 ;
-        unordered_map<char,int> mp ;
-        for ( int r = 0 ; r < n ; r++ ) {
-            while ( mp[s[r]] > 0 ) {
-                mp[s[l]]-- ;
-                l++ ;
+        // Approach 1 : Checking all substrings with HashSet 
+        // TC : O(N * N) , SC : O(1) 
+        int n = s.size() ;
+        int ans = 0 ;
+        for ( int i = 0 ; i < n ; i++ ) {
+            unordered_set<char> uset ;
+            for ( int j = i ; j < n ; j++ ) {
+                if ( uset.count(s[j]) ) break ; // this means duplicate found 
+                uset.insert(s[j]) ;
+                ans = max ( ans , j - i + 1 ) ;
             }
-            mp[s[r]]++ ;
-            ans = max ( ans , r - l + 1 ) ;
         }
         return ans ;
     }
