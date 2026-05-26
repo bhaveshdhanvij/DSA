1794a58c67a41566f813c3365e225f504727d7a6
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-26 09:27:03 +0530

    Time: 37 ms (76.34%) | Memory: 45.4 MB (71.09%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
+    // Microsoft
+    // Snapdeal
     int subarraySum(vector<int>& nums, int k) {
-        // TC : O(n) | SC : O(n) 
-        unordered_map<int,int> umap ;
-        umap[0] = 1 ;
+        // Naive Approach : Using 2 for loops
+        // TC : O(N * N) , SC : O(1) 
 
-        int ans = 0 ;
-        int sum = 0 ;
+        // int n = nums.size() ;   
+        // int count = 0 ;
+        // for ( int i = 0 ; i < n ; i++ ) {
+        //     int sum = 0 ;
+        //     for ( int j = i ; j < n ; j++ ) {
+        //         sum += nums[j] ;
+        //         if ( sum == k ) {
+        //             count++ ;
+        //         }
+        //     }
+        // }
+        // return count ;
 
-        for ( int i = 0 ; i < nums.size() ; i++ ) {
+
+        // Optimal Approach : Using Hashing + Prefix Sum  
+        // TC : O(N) , SC : O(N) 
+
+        int n = nums.size() ;
+        unordered_map<int,int> mp ;
+        int sum = 0 , ans = 0 ;
+        mp[0] = 1 ;
+        for ( int i = 0 ; i < n ; i++ ) {
             sum += nums[i] ;
-            if ( umap.count(sum - k) ) {
-                ans += umap[sum - k] ;
+            if ( mp.count(sum - k) ) {
+                ans += mp[sum - k] ;
             }
-            umap[sum]++ ;
+            mp[sum]++ ;
         }
-
         return ans ;
     }
 };
