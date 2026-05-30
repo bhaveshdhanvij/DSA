ecca2fad05d6055c82ed9c5f58ae67973077fa59
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-30 10:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (91.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
+    // Flipkart
+    // Directi
+    // Media.Net
+    // Google
+    // Adobe
+    // Nagarro
     bool isAnagram(string s, string t) {
-        if ( s.size() != t.size() ) return false ;
-    
-        vector<int> f(26 , 0) ;
-        for ( int i = 0 ; i < s.size() ; i++ ) {
-            f[s[i] - 'a']++ ;
-            f[t[i] - 'a']-- ;
-        }
+        // Approach 1 : Using Sorting and then comparing 
+        // TC : O(Nlog(N)) , SC : O(1) 
+
+        // sort ( s.begin() , s.end() ) ;
+        // sort ( t.begin() , t.end() ) ;
+        // return s == t ;
+
+        // Approach 2 :  Using HashMap 
+        // TC : O(N) , SC : O(N) 
+
+        // if ( s.length() != t.length() ) return false ;
+        // unordered_map<char,int> a , b ;
+        // for ( char c : s ) a[c]++ ;
+        // for ( char c : t ) b[c]++ ;
+        // return a == b ;
+
+        // Optimal Approach : Using alphabet frequency counting 
+        // TC : O(N) , SC : O(1) 
+
+        if ( s.length() != t.length() ) return false ;
+        int count[26] = {0} ;
+
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            count[s[i] - 'a']++ ;
+            count[t[i] - 'a']-- ;
+        }  
 
         for ( int i = 0 ; i < 26 ; i++ ) {
-            if ( f[i] != 0 ) return false ;
+            if ( count[i] != 0 ) {
+                return false ;
+            }
         }
-
         return true ;
     }
 };
