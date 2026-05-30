94da48b34c7312e8cfe0d099d236341bc85f3986
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-05-30 09:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (77.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
+    // Amazon
+    // DE Shaw
+    // Facebook
+    // Cisco
+    // FactSet
+    // Paytm
+    // Zoho
+    // Morgan Stanley
     bool isPalindrome(string s) {
-        // TC : O(N)  , SC : O(1) 
-        int i = 0 , j = s.size() - 1 ;
+        // Approach 1 : Using clean and then compare method 
+        // TC : O(N) , SC : O(N)
+ 
+        // int n = s.length() ;
+        // string a = "" ;
+        // for ( char c : s ) {
+        //     if ( isalnum(c) ) {
+        //         a += tolower(c) ;
+        //     }
+        // }
+        // string b = a ;
+        // reverse(b.begin() , b.end()) ;
+        // return a == b ;
+
+
+        // Optimal Approach : Using 2 - pointer approach 
+        // TC : O(N) , SC : O(1) 
+
+        int n = s.length() ;
+        int i = 0 , j = n - 1 ;
         while ( i < j ) {
             if ( !isalnum(s[i]) ) i++ ;
             else if ( !isalnum(s[j]) ) j-- ;
             else if ( tolower(s[i]) != tolower(s[j]) ) return false ;
             else {
-                i++ , j-- ;
+                i++ ;
+                j-- ;
             }
         }
-
         return true ;
     }
 };
