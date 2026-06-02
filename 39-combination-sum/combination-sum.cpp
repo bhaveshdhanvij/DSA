67b22b326b93ec404b26e85f51460d25aa826565
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-02 10:31:27 +0530

    Time: 3 ms (51.19%) | Memory: 14.1 MB (73.31%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> ans ;
-    vector<int> p ;
-    void helper(vector<int>& a , int t , int st ) {
-        if ( t == 0 ) {
-            ans.push_back(p) ;
+    // Microsoft
+    // Amazon
+    // DE Shaw
+    // Salesforce
+    // Oracle
+    // Adobe
+    void helper(vector<int>&a , int target , int st , vector<int>& curr , vector<vector<int>>& ans) {
+        if ( target == 0 ) {
+            ans.push_back(curr) ;
             return ;
         }
-
         for ( int i = st ; i < a.size() ; i++ ) {
-            if ( a[i] > t ) continue ;
-
-            p.push_back(a[i]) ;
-            helper( a , t - a[i] , i ) ;
-            p.pop_back() ;
+            if ( a[i] > target ) break ;
+            
+            curr.push_back(a[i]) ;
+            helper(a , target - a[i] , i , curr , ans ) ;
+            curr.pop_back() ;
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& a, int t) {
-        helper(a , t , 0) ;
+    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+        // Optimal Approach : Backtracking with repetition and early pruning 
+        // TC : O(2 ** N) , SC : O(target) 
+
+        sort ( a.begin() , a.end() ) ;
+        vector<vector<int>> ans ;
+        vector<int> curr ;
+        helper(a , target , 0 , curr , ans) ;
         return ans ;
     }
 };
