6171efd6d74a97881d01936503bac09ba9fd5a0a
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-06 13:09:52 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (14.10%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public: 
+    // Apple
+    // Microsoft
+    // Meta
+    // Swiggy
+    // Amazon
+    // Google
+    // Oracle
+    // Morgan Stanley
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // Optimal Approach : Calculate the NG for every element in nums2 and then map it for nums1 ans storage
+        
+        unordered_map<int,int> mp ; // nums2 -> NG 
+        stack<int> s ;
+
+        for ( int i = nums2.size() - 1 ; i >= 0 ; i-- ) {
+            while (!s.empty() && s.top() <= nums2[i]) {
+                s.pop() ;
+            }     
+
+            if ( s.empty() ) {
+                mp[nums2[i]] = -1 ;
+            }else {
+                mp[nums2[i]] = s.top() ;
+            }
+
+            s.push(nums2[i]) ;
+        }
+
+        vector<int> ans ;
+        
+        for ( int i = 0 ; i < nums1.size() ; i++ ) {
+            ans.push_back(mp[nums1[i]]) ;
+        }
+
+        return ans ;
+    }
+};
