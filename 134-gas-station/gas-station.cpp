703c32339a576fde41fd025aeb3cea83668d5574
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-08 12:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (59.64%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    // Salesforce
+    // Adobe
+    // Oracle
+    // Google
+    // Microsoft
+    // Amazon
+    // Meta
+    // Cisco
+    // Bank of Newyork
+    // Infosys
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int c = 0 , g = 0 ;
+        // Optimal Approach : Greedy index eliminaton technique by calculating rem gas after the move 
+        // TC : O(N) , SC : O(1) 
+
         int n = gas.size() ;
+        int c = 0 , g = 0 , curr = 0 , st = 0 ;
+
         for ( int i = 0 ; i < n ; i++ ) {
             c += cost[i] ;
             g += gas[i] ;
-        }
-        if ( c > g ) return -1 ;
-        int st = 0 , x = 0 ;
-        for ( int i = 0 ; i < n ; i++ ) {
-            x += gas[i] - cost[i] ;
-            if (x < 0) {
-                x = 0 ;
+
+            curr += (gas[i] - cost[i]) ;
+
+            if ( curr < 0 ) { // existing starting index is invalid to start 
                 st = i + 1 ;
+                curr = 0 ;
             }
         }
-        return st ;
+
+        return g < c ? -1 : st ; // ensuring total gas is always >= total cost 
     }
 };
