ca5fb60086702361c8a556a22c8ab3a6a1cd4458
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-08 11:00:11 +0530

    Time: 1 ms (37.80%) | Memory: 16.8 MB (69.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& a) {
-        // Optimal Approach : BFS (parallel processing)
+    // Amazon
+    // Flipkart
+    // Intuit
+    // Samsung
+    // Oracle
+    // Meta
+    // Phone Pe
+    // Microsoft
+    // Google
+    int orangesRotting(vector<vector<int>>& a) { // grid -> a
+        // Optimal Approach : Multi Source BFS 
         // TC : O(N * M) , SC : O(N * M)
-        // BFS used since every rotten orange is making surrounding fresh orange rotten 
+
         int n = a.size() , m = a[0].size() ;
-        queue<pair<int,int>> q ;
         
-        int f = 0 ; // count of fresh oranges 
+        queue<pair<int,int>> q ; // for storing indices of rotten oranges as a pair
+        int f = 0 ; // count of fresh orange 
+
+        int t = 0 ; // No. of time until all fresh oranges become rotten 
 
         for ( int i = 0 ; i < n ; i++ ) {
             for ( int j = 0 ; j < m ; j++ ) {
-                if ( a[i][j] == 2 ) q.push({i , j}) ;
-                else if ( a[i][j] == 1 ) f++ ;
+                if ( a[i][j] == 1 ) f++ ;
+                else if ( a[i][j] == 2 ) {
+                    q.push({i , j}) ;
+                } 
             }
         }
 
-        int delRow[] = {-1 , 0 , 1 , 0} ;
-        int delCol[] = {0 , 1 , 0 , -1} ;
+        if ( f == 0 ) return 0 ;
 
-        int t = 0 ; // time taken ie ans
+        int delRow[4] = {-1 , 0 , 1 , 0} ;
+        int delCol[4] = {0 , 1 , 0 , -1} ;
 
-        while ( !q.empty() && f > 0 ) {
-            int s = q.size() ; // current no of rotten oranges (curr level)
-            t++ ;
-
-            while (s--) {
-                auto p = q.front() ;
+        while(!q.empty() && f > 0) {
+            int size = q.size() ;
+            while (size--) {
+                auto x = q.front() ;
                 q.pop() ;
-
-                int i = p.first , j = p.second ;
-
-                for ( int k = 0 ; k < 4 ; k++ ) {
-                    int nr = i + delRow[k] ;
-                    int nc = j + delCol[k] ;
+                
+                int r = x.first , c = x.second ;
+                
+                for ( int i = 0 ; i < 4 ; i++ ) {
+                    int nr = r + delRow[i] ;
+                    int nc = c + delCol[i] ;
                     
                     if ( nr >= 0 && nr < n && nc >= 0 && nc < m && a[nr][nc] == 1 ) {
                         f-- ;
@@ -42,8 +53,9 @@ public:
                     }
                 }
             }
+            t++ ;
         }
 
-        return f == 0 ? t : -1 ; // checking if all fresh oranges became rotten or not 
+        return f == 0 ? t : -1 ; // if some fresh orange remain then -1 , else time taken 
     }
 };
