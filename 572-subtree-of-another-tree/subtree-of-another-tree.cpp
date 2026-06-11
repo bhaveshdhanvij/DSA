04bc2bd6dd87e65990f3a08dd53f398980b88065
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-11 11:57:44 +0530

    Time: 3 ms (26.48%) | Memory: 28.9 MB (70.24%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Microsoft
+    // Uber
+    // Ebay
+    // Google
+    // Amazon
+    // Morgan Stanley
+    bool same(TreeNode* a , TreeNode* b) {
+        if ( !a && !b ) return true ;
+        if ( !a || !b || a->val != b->val ) return false ;
+
+        return same(a->left , b->left) && same(a->right , b->right) ;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // Recursive code : find out matching values of subroot's root in the main tree and use the identical tree function recursively 
+        // TC : O(N * M) , SC : O(Ha * Hb)
+
+        if ( root == nullptr || subRoot == nullptr ) {
+            return root == subRoot ;
+        }
+
+        if ( root->val == subRoot->val && same(root , subRoot) ) {
+            return true ;
+        }
+
+        return isSubtree(root->left , subRoot) || isSubtree(root->right , subRoot) ;
+    }
+};
