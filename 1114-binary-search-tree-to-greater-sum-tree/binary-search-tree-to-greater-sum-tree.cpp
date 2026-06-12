2b31f8b1b5db0d8948454b282636930bad485063
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-12 13:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (46.03%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -11,21 +11,26 @@
  */
 class Solution {
 public:
+    // Amazon
+    // Microsoft
+    // Ebay
+    // SAP Labs
     int sum = 0 ;
-
     void helper( TreeNode* root ) {
-        if ( !root ) return  ;
-
+        if (!root) return ;
+        
         helper(root->right) ;
 
         sum += root->val ;
-
         root->val = sum ;
 
         helper(root->left) ;
     }
     TreeNode* bstToGst(TreeNode* root) {
-        helper( root ) ;
+        // Approach : Reverse inorder traversal : updates greater sum automatically 
+        // TC : O(N) , SC : O(N)
+        
+        helper(root) ;
         return root ;
     }
 };
