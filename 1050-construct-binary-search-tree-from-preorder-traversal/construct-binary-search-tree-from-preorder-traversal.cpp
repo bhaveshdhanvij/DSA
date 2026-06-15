9c8e030ecbd2b6c98151d735eee4b0f955c31b55
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-15 12:17:44 +0530

    Time: 3 ms (17.96%) | Memory: 16.3 MB (69.38%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,21 +11,28 @@
  */
 class Solution {
 public:
+    // Adobe
+    // Google
+    // Microsoft
     TreeNode* helper(vector<int> &preorder , int &idx , int maxx ) {
         if ( idx == preorder.size() || preorder[idx] > maxx ) {
-            return nullptr ;
+            return NULL ;
         }
 
-        int currval = preorder[idx++] ;
-        TreeNode* root = new TreeNode(currval) ;
+        int curr = preorder[idx++] ;
 
-        root->left = helper(preorder, idx , currval) ;
-        root->right = helper(preorder , idx , maxx) ;
-        
+        TreeNode* root = new TreeNode(curr) ;
+
+        root->left = helper(preorder , idx , curr ) ;
+        root->right = helper(preorder , idx , maxx ) ;
+
         return root ;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
+        // Approach : Using BST property range logic 
+        // TC : O(N) , SC : O(h) - height 
+        
         int idx = 0 ;
-        return helper(preorder, idx , INT_MAX) ;
+        return helper(preorder , idx , INT_MAX) ;
     }
 };
