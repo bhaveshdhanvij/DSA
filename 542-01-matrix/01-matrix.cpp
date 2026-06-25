c7ed1d540bcbda6968b48345959a3e80a8c23ae1
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-06-25 22:13:59 +0530

    Time: 15 ms (72.19%) | Memory: 34.7 MB (76.61%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
+    // Linkedin
+    // Doordash
+    // Google
+    // Amazon
+    // Flipkart
+    // Adobe
     vector<vector<int>> updateMatrix(vector<vector<int>>& a) {
-        int n = a.size() , m = a[0].size() ;
-        vector<vector<int>> ans(n , vector<int> (m , -1)) ;
-        
+        int m = a.size() , n = a[0].size() ;
+        vector<vector<int>> d(m , vector<int>(n , INT_MAX)) ;
+        // firstly set all 0 => 0's  and all 1's => INF
+
         queue<pair<int,int>> q ;
-        for ( int i = 0 ; i < n ; i++ ) {
-            for ( int j = 0 ; j < m ; j++ ) {
+        for ( int i = 0 ; i < m ; i++ ) {
+            for ( int j = 0 ; j < n ; j++ ) {
                 if ( a[i][j] == 0 ) {
-                    ans[i][j] = 0 ;
+                    d[i][j] = 0 ;
                     q.push({i , j}) ;
                 }
             }
         }
 
-        int delRow[] = {-1 , 0 , 1 , 0} ;
-        int delCol[] = {0 , 1 , 0 , -1} ;
-
-        while ( !q.empty() ) {
-            auto [x , y] = q.front() ;
+        int delRow[4] = {-1 , 0 , 1 , 0} ;
+        int delCol[4] = {0 , 1 , 0 , -1} ;
+        
+        while (q.size() > 0) {
+            auto [r , c] = q.front() ;
             q.pop() ;
 
             for ( int i = 0 ; i < 4 ; i++ ) {
-                int nx = x + delRow[i] ;
-                int ny = y + delCol[i] ;
-                
-                if ( nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] == -1 ) {
-                    ans[nx][ny] = 1 + ans[x][y] ;
-                    q.push({nx , ny}) ;
+                int nr = r + delRow[i] ;
+                int nc = c + delCol[i] ;
+
+                if ( nr >= 0 && nr < m && nc >= 0 && nc < n && d[nr][nc] > d[r][c] + 1 ) {
+                    d[nr][nc] = d[r][c] + 1 ;
+                    q.push({nr , nc}) ;
                 }
             }
         }
 
-        return ans ;
+        return d ;
     }
 };
