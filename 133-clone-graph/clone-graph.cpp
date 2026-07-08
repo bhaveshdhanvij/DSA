61637d141591837b717bbc6ece036d5756326c07
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-07-08 14:04:49 +0530

    Time: 3 ms (68.41%) | Memory: 12.2 MB (48.54%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -21,13 +21,27 @@ public:
 
 class Solution {
 public:
+    // Google
+    // Google
+    // Amazon
+    // Amazon
+    // Meta
+    // Meta
+    // Uber
+    // Uber
+    // Adobe
+    // Adobe
     unordered_map<Node*,Node*> mp ;
     Node* cloneGraph(Node* node) {
+        // Optimal Approach : Using HashMap + DFS 
+        // TC : O(V + E) , SC : O(V)
+        
         if (!node) return nullptr ;
-
+        
         if ( mp.count(node) ) return mp[node] ;
-
+        
         Node* clone = new Node(node->val) ;
+        
         mp[node] = clone ;
 
         for ( Node* n : node->neighbors ) {
