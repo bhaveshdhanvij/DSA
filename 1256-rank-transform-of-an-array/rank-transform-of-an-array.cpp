54894ed69030079655ddbb01b9b3543925117503
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-07-12 09:08:03 +0530

    Time: 16 ms (98.94%) | Memory: 39.6 MB (96.46%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& a) {
         int n = a.size() ;
-        vector<int> ans(n) ;
-
         vector<pair<int,int>> p ;
+        
         for ( int i = 0 ; i < n ; i++ ) {
             p.push_back({a[i] , i}) ;
         }
 
+        vector<int> ans(n) ;
         sort ( p.begin() , p.end() ) ;
-        int r = 1 ;
 
+        int r = 1 ;
+        
         for ( int i = 0 ; i < n ; i++ ) {
-            int idx = p[i].second ;
             int x = p[i].first ;
+            int idx = p[i].second ;
             
-            if ( i > 0 && p[i].first == p[i - 1].first ) {
+            if ( i > 0 && x == p[i - 1].first ) {
                 ans[idx] = ans[p[i - 1].second] ;
             }else {
                 ans[idx] = r ;
                 r++ ;
-            } 
+            }
         }
 
         return ans ;
