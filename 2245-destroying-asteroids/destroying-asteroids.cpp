6fc00d99c4e9103f647c04bec42aa9c8d4c2259d
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-07-29 19:47:46 +0530

    Time: 32 ms (50.57%) | Memory: 106.9 MB (16.34%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int m, vector<int>& a) {
-        long long t = m ;
-        int n = a.size() ;
-        sort ( a.begin() , a.end() ) ;
-        for ( int i = 0 ; i < n ; i++ ) {
-            if ( t < a[i] ) {
-                return false ;
-            }
-            t += a[i] ;
+        long long curr = m ;
+        sort(a.begin() , a.end()) ;
+        for (int x : a) {
+            if ( x > curr ) return false ;
+            curr += x ;
         }
         return true ;
     }
