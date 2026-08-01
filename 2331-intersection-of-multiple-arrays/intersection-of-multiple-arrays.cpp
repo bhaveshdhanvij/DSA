7b9a3be9e9465ce8c8eb1ca84113ea00c0f50e1f
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-08-01 11:18:41 +0530

    Time: 4 ms (37.36%) | Memory: 16.9 MB (32.25%) - LeetSync

---
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        int n = nums.size() ;
+        unordered_map<int,int> mp ;
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < nums[i].size() ; j++ ) {
+                mp[nums[i][j]]++ ;
+            }
+        }
+        vector<int> ans ;
+        for ( auto p : mp ) {
+            if ( p.second == n ) {
+                ans.push_back(p.first) ;
+            }
+        }
+        sort(ans.begin() , ans.end()) ;
+        return ans ;
+    }
+};
