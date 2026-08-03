b12f5eb0bb8fc2fae13a63d478959cc857c4e094
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-08-03 21:22:36 +0530

    Time: 139 ms (92.27%) | Memory: 239.1 MB (91.56%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,33 +1,36 @@
+class DSU {
+public:
+    vector<int> parent , size ;
+    DSU(int n) {
+        parent.resize(n) ;
+        size.resize(n , 1) ;
+        for ( int i = 0 ; i < n ; i++ ) {
+            parent[i] = i ;
+        }
+    }
+
+    int find(int x) {
+        if ( parent[x] == x ) return x ;
+        return parent[x] = find(parent[x]) ; // path compresssion 
+    }
+
+    void unite(int a , int b) {
+        a = find(a) ;
+        b = find(b) ;
+        if ( a == b ) return ;
+        if ( size[a] < size[b] ) swap(a ,b) ;
+        parent[b] = a ;
+        size[a] += size[b] ;
+    }
+};
+
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
-        vector<vector<int>> a(n) ;
-        
-        for ( auto &e : edges ) {
-            a[e[0]].push_back(e[1]) ;
-            a[e[1]].push_back(e[0]) ;
+        DSU dsu(n) ;
+        for (auto e : edges) {
+            dsu.unite(e[0] , e[1]) ;
         }
-
-        vector<bool> vis(n , false) ;
-        queue<int> q ;
-
-        q.push(s) ;
-        vis[s] = true ;
-
-        while (!q.empty()) {
-            int node = q.front() ;
-            q.pop() ;
-
-            if ( node == d ) return true ;
-
-            for ( int n : a[node] ) {
-                if (!vis[n]) {
-                    vis[n] = true ;
-                    q.push(n) ;
-                }
-            }
-        }
-
-        return false ;
+        return dsu.find(s) == dsu.find(d) ;
     }
 };
