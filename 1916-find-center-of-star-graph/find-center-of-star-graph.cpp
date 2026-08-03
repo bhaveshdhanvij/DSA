9e965c7cd2af286a0d240605182f60b6dbc90ecf
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-08-03 21:29:21 +0530

    Time: 58 ms (20.92%) | Memory: 116.9 MB (25.32%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,19 +1,43 @@
+class DSU {
+public:
+    vector<int> parent , size ;
+    DSU(int n) {
+        parent.resize(n + 1) ;
+        size.resize(n + 1, 1) ;
+        for ( int i = 1 ; i <= n ; i++ ) {
+            parent[i] = i ;
+        }
+    }
+
+    int find(int x) {
+        if ( parent[x] == x ) return x ;
+        return parent[x] = find(parent[x]) ; // path compression 
+    }
+
+    void unite(int a , int b) {
+        a = find(a) ;
+        b = find(b) ;
+        if (a == b) return ;
+        if ( size[a] < size[b] ) swap(a , b) ;
+        parent[b] = a ;
+        size[a] += size[b] ;
+    }
+};
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        
         int n = edges.size() + 1 ;
-        vector<vector<int>> a(n + 1) ;
-
-        for( auto &e : edges ) {
-            a[e[0]].push_back(e[1]) ;
-            a[e[1]].push_back(e[0]) ;
+        DSU dsu(n) ;
+        vector<int> d(n + 1 , 0) ;
+        for ( auto e : edges ) {
+            d[e[0]]++ ;
+            d[e[1]]++ ;
         }
-
-        for ( int i = 1 ; i <= n ; i++ ) {
-            if ( a[i].size() == n - 1 ) return i ;
+        for ( int i =1 ; i <= n ; i++ ) {
+            if ( d[i] == n - 1 ) {
+                return i ;
+            }
         }
-
         return -1 ;
     }
 };
