8f395c30a0411058539ee3fb86787ff7a3972cfc
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-08-09 12:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (5.11%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,104 +1,63 @@
-// class Node {
-// public:
-//     Node* child[26] ;
-//     int c ;
-//     Node() {
-//         for ( int i = 0 ; i < 26 ; i++ ) {
-//             child[i] = nullptr ;
-//         }
-//         c = 0 ;
-//     }
-// };
+class RollingHash {
+private:
+    using ll = long long ;
+    static const ll mod = 1e9 + 7 ;
+    static const ll base = 911382323 ;
+    vector<ll> pref , power ;
+public:
+    RollingHash(const string& s) {
+        int n = s.size() ;
+        pref.resize(n + 1 , 0) ;
+        power.resize(n + 1 , 1) ;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            power[i + 1] = power[i] * base % mod ;
+            pref[i + 1] = (pref[i] * base + (s[i] - 'a' + 1)) % mod ;
+        }
+    }
+
+    ll get(int l , int r) {
+        if ( l > r ) return 0 ;
+        return ( pref[r + 1] - pref[l] * power[r - l + 1] % mod + mod) % mod ;
+    }
+};
+
 class Solution {
 public:
-    // void insert(Node* root , string& s ) {
-    //     Node* node = root ;
-        
-    //     for ( int i = 0 ; i < s.size() ; i++ ) {
-    //         int idx = s[i] - 'a' ;
-    //         if ( node->child[idx] == nullptr ) {
-    //             node->child[idx] = new Node() ;
-    //         }
-    //         node = node->child[idx] ;
-    //         node->c++ ;
-    //     }
-    // }
-    // string search(Node* root , int n) {
-    //     Node* node = root ;
-    //     string ans = "" ;
-
-    //     while ( true ) {
-    //         int cnt = 0 ;
-    //         int idx = -1 ;
-
-    //         for ( int i = 0 ; i < 26 ; i++ ) {
-    //             if ( node->child[i] != nullptr && node->child[i]->c == n ) {
-    //                 cnt++ ; 
-    //                 idx = i ;
-    //             }
-    //         }
-
-    //         if ( cnt != 1 ) break ;
-    //         ans.push_back(char(idx + 'a')) ;
-    //         node = node->child[idx] ;
-    //     }
-    //     return ans ;
-    // }
     string longestCommonPrefix(vector<string>& a) {
-        // int n = strs.size() ;
-        // if ( n == 0 ) return "" ;
-
-        // Node* root = new Node() ;
+        int n = a.size() ;
+        if ( n == 0 ) return "" ;
+        int minn = INT_MAX ;
+        for ( string s : a ) minn = min ( minn , (int)s.size()) ;
         
-        // for ( string s : strs ) {
-        //     insert(root , s) ;
-        // }
-        
-        // return search(root , n) ;
-
-
-        // Approach 1 : Horizontal Scalling 
-        // TC : O(N * M) , SC : O(1) 
-
-        // if (a.empty()) return "" ;
-        // string p = a[0] ;
-        
-        // for ( int i = 1 ; i < a.size() ; i++ ) {
-        //     while ( a[i].find(p) != 0 ) {
-        //         p.pop_back() ;
-        //         if ( p.empty() ) {
-        //             return "" ;
-        //         }
-        //     }
-        // }
-        // return p ;
-
-        // Approach 2 : Vertical Scanning 
-        // TC : O(N * M) , SC : O(1) 
-
-        // if (a.empty()) return "" ;
-        // for ( int i = 0 ; i < a[0].size() ; i++ ) {
-        //     char ch = a[0][i] ;
-        //     for ( int j = 1 ; j < a.size() ; j++ ) {
-        //         if ( i >= a[j].size() || a[j][i] != ch ) {
-        //             return a[0].substr(0 , i) ;
-        //         }
-        //     }
-        // }
-        // return a[0] ;
-
-
-        // Optimal Approach : Sorting + Greedy 
-        // TC : O(Nlog(N)) , SC : O(1) 
-
-        sort ( a.begin() , a.end() ) ;
-        string x = a.front() ;
-        string y = a.back() ;
-        
-        int i = 0 ;
-        while ( i < x.size() && i < y.size() && x[i] == y[i] ) {
-            i++ ;
+        vector<RollingHash> hashes ;
+        for ( const string& s : a ) {
+            hashes.emplace_back(s) ;
         }
-        return x.substr(0 , i) ;
+
+        auto check = [&](int len){
+            if ( len == 0 ) return true ;
+            
+            long long t = hashes[0].get(0 , len - 1) ;
+            for ( int i = 1 ; i < n ; i++ ) {
+                long long curr = hashes[i].get(0 , len - 1) ;
+                if ( curr != t ) {
+                    return false ;
+                }
+            }
+            return true ;
+        };
+
+        int l = 0 , h = minn ;
+        while ( l < h ) {
+            int mid = l + (h - l + 1) / 2 ;
+            if ( check(mid) ) {
+                l = mid ;
+            }else {
+                h = mid - 1 ;
+            }
+        }
+
+        return a[0].substr(0 , l) ;
     }
 };
