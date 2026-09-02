0b7cae85260000ffc8a35aa47239a9204b3d1a2c
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-09-02 09:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (99.86%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& a) {
-        return true ;
+    bool uniformArray(vector<int>& nums1) {
+        bool flag = true , odd = false ;
+        for (int x : nums1 ) {
+            if ( x % 2 != 0 ) {
+                flag = false ;
+                odd = true ;
+            }
+        }
+        return flag || odd ;
     }
 };
