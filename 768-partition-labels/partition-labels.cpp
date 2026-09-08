d8a9237e93977a707a7f1c8414fd0ce3723b441d
Bhavesh D <163393683+bhaveshdhanvij@users.noreply.github.com>
2026-09-08 20:48:15 +0530

    Time: 4 ms (10.03%) | Memory: 8.9 MB (39.43%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -4,13 +4,10 @@ public:
         int n = s.size() ;
         vector<int> last(26) ;
         vector<int> ans ;
-        
-        for ( int i = 0 ; i < n ; i++ ) {
-            last[s[i] - 'a'] = i ;
-        }
+        for ( int i = 0 ; i < n ; i++ ) last[s[i] - 'a'] = i ;
         int st = 0 , end = 0 ;
         for ( int i = 0 ; i < n ; i++ ) {
-            end = max ( end , last[s[i] - 'a'] ) ;
+            end = max(end , last[s[i]- 'a']) ;
             if ( i == end ) {
                 ans.push_back(end - st + 1) ;
                 st = i + 1 ;
